25a6af44e7729904f1d1847f5d0252e154359af0
Z-C-Code <zachary.lights@gmail.com>
2021-11-28 16:55:31 -0800

    drop icon

--- public/main.js
+++ public/main.js
@@ -1,12 +1,1 @@
 function menu() {var x = document.getElementById("myTopnav");if (x.className === "topnav") {x.className += " responsive";} else {x.className = "topnav";}}
-
-function showsearch() {
-  document.getElementById("search").style.display = "block";
-  document.getElementById("SearchIcon").style.display = "none";
-  document.getElementById("CloseIcon").style.display = "block";
-}
-function hidesearch() {
-  document.getElementById("search").style.display = "none";
-  document.getElementById("SearchIcon").style.display = "block";
-  document.getElementById("CloseIcon").style.display = "none";
-}
