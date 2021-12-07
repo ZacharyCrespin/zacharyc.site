9ad2ae6bfc8059b4ae0253bb5ab936b1fdf80ba5
Z-C-Code <zachary.lights@gmail.com>
2021-12-07 15:47:00 -0800

    get rid of search

--- public/main.js
+++ public/main.js
@@ -2,13 +2,3 @@ function menu() {var x = document.getElementById("myTopnav");if (x.className ===
 window.onload = function() {
     document.getElementById("loader").style.display = "none";
 };
-
-function openSearch() {
-    document.getElementById("searchBox").style.display = "block";
-    document.getElementById("searchIcon").style.display = "none";
-    document.getElementById("myTopnav").className = "topnav";
-};
-function closeSearch() {
-    document.getElementById("searchBox").style.display = "none";
-    document.getElementById("searchIcon").style.display = "block";
-};
