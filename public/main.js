152f3fbe5a08fcc1c9a7957aed8519138fec7458
Z-C-Code <zachary.lights@gmail.com>
2021-12-06 17:58:02 -0800

    2.2

--- public/main.js
+++ public/main.js
@@ -1,2 +1,14 @@
 function menu() {var x = document.getElementById("myTopnav");if (x.className === "topnav") {x.className += " responsive";} else {x.className = "topnav";}}
-window.onload = function() {document.getElementById("loader").style.display = "none";};
+window.onload = function() {
+    document.getElementById("loader").style.display = "none";
+};
+
+function openSearch() {
+    document.getElementById("searchBox").style.display = "block";
+    document.getElementById("searchIcon").style.display = "none";
+    document.getElementById("myTopnav").className = "topnav";
+};
+function closeSearch() {
+    document.getElementById("searchBox").style.display = "none";
+    document.getElementById("searchIcon").style.display = "block";
+};
