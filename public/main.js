68220e567cca2490546329b4b1d8feb366da55ef
Z-C-Code <zachary.lights@gmail.com>
2021-12-24 08:00:16 -0800

    2.4

--- public/main.js
+++ public/main.js
@@ -5,6 +5,10 @@ function menu() {
 window.onload = function() {
     document.getElementById("loader").style.display = "none";
 };
+function drop() {
+    var x = document.getElementById("drop");
+    if (x.className === "dropdown") {x.className += "-open";} else {x.className = "dropdown";}
+}
 function searchOpen() {
     document.getElementById("search").style.display = "block";
     document.getElementById("searchIcon").style.display = "none"
