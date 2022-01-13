12ee41297b61f8378cca5226b220135ee6f6b62b
Z-C-Code <zachary.lights@gmail.com>
2022-01-12 18:25:45 -0800

    3.2

--- public/main.js
+++ public/main.js
@@ -6,14 +6,10 @@ window.onload = function() {
     document.getElementById("loader").style.display = "none";
 };
 function drop() {
-    var x = document.getElementById("drop");
+    var x = document.getElementById("dropdown");
     if (x.className === "dropdown") {x.className += "-open";} else {x.className = "dropdown";}
 }
-function searchOpen() {
-    document.getElementById("search").style.display = "block";
-    document.getElementById("searchIcon").style.display = "none"
-}
-function searchClose() {
-    document.getElementById("search").style.display = "none";
-    document.getElementById("searchIcon").style.display = "block"
+function openSearch() {
+    var x = document.getElementById("search");
+    if (x.className === "search") {x.className += "-open";} else {x.className = "search";}
 }
