fad1438004f7c08885cc88a3b62b800880429741
Z-C-Code <zachary.lights@gmail.com>
2022-02-19 09:47:43 -0800

    3.2

--- public/main.js
+++ public/main.js
@@ -4,39 +4,22 @@ window.onload = function() {
     // Update favicon for dark mode
     const darkModeListener = (event) => {
     if (event.matches) {
-        document.getElementById('icon').setAttribute("href","imgs/favicon-white.png");
+        document.getElementById('favicon').setAttribute("href","https://zacharyc.site/imgs/favicon-white.png");
     } else {
-        document.getElementById('icon').setAttribute("href","imgs/favicon-black.png");
+        document.getElementById('favicon').setAttribute("href","https://zacharyc.site/imgs/favicon-black.png");
     }
     };
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', darkModeListener);
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.getElementById('icon').setAttribute("href","imgs/favicon-white.png");
+        document.getElementById('favicon').setAttribute("href","https://zacharyc.site/imgs/favicon-white.png");
     } else {
-        document.getElementById('icon').setAttribute("href","imgs/favicon-black.png");
+        document.getElementById('favicon').setAttribute("href","https://zacharyc.site/imgs/favicon-black.png");
     }
-
-    // if (localStorage.getItem('Theme') == 'Dark') {
-    //     dark()
-    // }
-    // if (localStorage.getItem('Theme') == 'Light') {
-    //     light()
-    // }
 
     if ("serviceWorker" in navigator) {
         navigator.serviceWorker.register("service-worker.js");
     }
 }
-// function light() {
-//     localStorage.setItem('Theme',  'Light');
-//     document.documentElement.style.setProperty('--white', '#000000');
-//     document.documentElement.style.setProperty('--black', '#FFFFFF');
-// }
-// function dark() {
-//     localStorage.setItem('Theme',  'Dark');
-//     document.documentElement.style.setProperty('--white', '#FFFFFF');
-//     document.documentElement.style.setProperty('--black', '#000000');
-// }
 function menu() {
     var x = document.getElementById("menu");
     if (x.className === "menu") {x.className += " open";} else {x.className = "menu";}
