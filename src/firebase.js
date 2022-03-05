288b7b80160edc92c0d68f64a4edc51facf7a0a8
Zachary C <Zachary.lights@gmail.com>
2022-03-05 15:46:07 -0800

    4.1.3

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getPerformance } from "firebase/performance";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAusVefSFH7pEqWLclTEDyEYFSlpMrzubk",
+  authDomain: "zacharyc.site",
+  databaseURL: "https://zacharycsite-default-rtdb.firebaseio.com",
+  projectId: "zacharycsite",
+  storageBucket: "zacharycsite.appspot.com",
+  messagingSenderId: "18418783539",
+  appId: "1:18418783539:web:904df4549700d037dc9a46",
+  measurementId: "G-7TQEN9M9FK"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const perf = getPerformance(app);
