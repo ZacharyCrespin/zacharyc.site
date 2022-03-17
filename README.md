ca28bc8ca7d4168950b5c8e9826e5eaa8ea54c2b
Zachary C <Zachary.lights@gmail.com>
2022-03-17 13:22:08 -0700

    4.2.1

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# zacharyc.site
-My Personal Website
+[![Netlify Status](https://api.netlify.com/api/v1/badges/3cb697b4-955b-47b5-b622-22f1ac65bbb1/deploy-status)](https://app.netlify.com/sites/zacharyc/deploys)
+# My Personal Website
 [zacharyc.site](https://zacharyc.site)
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getPerformance } from "firebase/performance";
 
 const firebaseConfig = {
@@ -13,5 +12,4 @@ const firebaseConfig = {
   measurementId: "G-7TQEN9M9FK"
 };
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const perf = getPerformance(app);
