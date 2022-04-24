2500fad2eb8e81e8e17311b0d416e0909bf4e46d
Zachary C <Zachary.lights@gmail.com>
2022-04-23 17:42:55 -0700

    4.4.2

--- src/code/advice/script.js
+++
@@ -1,7 +0,0 @@
-fetch("https://api.adviceslip.com/advice")
-.then(response => {
-    return response.json();
-})
-.then(response => {
-    document.getElementById('advice').innerHTML = response.slip.advice;
-})
