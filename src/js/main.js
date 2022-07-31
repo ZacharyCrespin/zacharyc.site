e19551f9053223bd04c5879a8979b2fac22a4382
Zachary Crespin <Zachary.lights@gmail.com>
2022-07-31 12:33:43 -0700

    add stylelint

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,8 @@
 window.onload = function() {
-    // Update Favicon for Dark Mode
-    const darkModeListener = (event) => {
-    if (event.matches) {
-        document.getElementById('favicon').setAttribute("href","/images/favicon-white.png");
-    } else {
-        document.getElementById('favicon').setAttribute("href","/images/favicon-black.png");
-    }
-    };
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', darkModeListener);
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.getElementById('favicon').setAttribute("href","/images/favicon-white.png");
-    } else {
-        document.getElementById('favicon').setAttribute("href","/images/favicon-black.png");
-    }
-
     // Register Service Worker
     if ("serviceWorker" in navigator) {
         navigator.serviceWorker.register("https://zacharyc.site/js/service-worker.js");
     }
-
     // Hide Loader
     document.getElementById("loader").style.display = "none";
 }
