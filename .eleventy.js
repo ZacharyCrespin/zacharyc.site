5f1a57c3f5a8c0e3b0f24e2310d017446fff7fa4
Zachary Crespin <Zachary.lights@gmail.com>
2022-08-21 07:59:03 -0700

    fix webmanifest

--- .eleventy.js
+++ .eleventy.js
@@ -15,7 +15,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/js');
     eleventyConfig.addPassthroughCopy('./src/favicon.ico');
     eleventyConfig.addPassthroughCopy('./src/insert.js');
-    eleventyConfig.addPassthroughCopy('./src/manifest.json');
+    eleventyConfig.addPassthroughCopy('./src/app.webmanifest');
     eleventyConfig.addPassthroughCopy('./src/*.html');
     eleventyConfig.addPassthroughCopy('./src/robots.txt');
     eleventyConfig.addPassthroughCopy('./src/sitemap.xml');
