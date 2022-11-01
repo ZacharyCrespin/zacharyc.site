f957428553a15bbb66637bb9831686df57261cc0
Zachary Crespin <Zachary.lights@gmail.com>
2022-11-01 16:47:34 -0700

    fix consent

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,19 @@
-function consent() {
-  const instance = ackeeTracker.create('https://analytics.zacharyc.site', {
-    detailed: true
-  });
-  instance.record('133c9354-5976-48e5-803f-809c8aeeda11');
-}
-function noConsent() {
-  const instance = ackeeTracker.create('https://analytics.zacharyc.site', {
-    detailed: false
-  });
-  instance.record('133c9354-5976-48e5-803f-809c8aeeda11');
-}
 function acceptCookies() {
-  localStorage.setItem('cookies', true);
-  consent();
-  document.getElementById('cookie-consent').style.display = 'none';
+  localStorage.setItem('consent', true);
+  document.getElementById('consent').style.display = 'none';
+  const instance = ackeeTracker.create('https://analytics.zacharyc.site', {detailed: true});
+  instance.record('133c9354-5976-48e5-803f-809c8aeeda11');
 }
-function declineCookies() {
-  localStorage.setItem('cookies', false);
-  noConsent();
-  document.getElementById('cookie-consent').style.display = 'none';
+function rejectCookies() {
+  localStorage.setItem('consent', false);
+  document.getElementById('consent').style.display = 'none';
 }
 
 window.onload = function onload() {
-  if (localStorage.getItem('cookies') == 'true') {
-    consent();
+  if (localStorage.getItem('consent') == 'true') {
+    const instance = ackeeTracker.create('https://analytics.zacharyc.site', {detailed: true});
+    instance.record('133c9354-5976-48e5-803f-809c8aeeda11');
   } else {
-    noConsent();
+    document.getElementById('consent').style.display = ''
   }
 };
