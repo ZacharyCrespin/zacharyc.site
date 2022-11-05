4b8e63165ec29b001eb1c1249462aa35f8c84c9f
Zachary Crespin <Zachary.lights@gmail.com>
2022-11-04 18:58:01 -0700

    update consent

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-function acceptCookies() {
+function acceptTracking() {
   localStorage.setItem('consent', true);
   document.getElementById('consent').style.display = 'none';
   const instance = ackeeTracker.create('https://analytics.zacharyc.site', {detailed: true});
   instance.record('133c9354-5976-48e5-803f-809c8aeeda11');
 }
-function rejectCookies() {
+function rejectTracking() {
   localStorage.setItem('consent', false);
   document.getElementById('consent').style.display = 'none';
 }
