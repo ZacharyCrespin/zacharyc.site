97ebb655341381da92536057f8a638d3b31a400c
Zachary Crespin <Zachary.lights@gmail.com>
2023-06-19 10:07:09 -0700

    add search

---
+++ functions/search.js
@@ -0,0 +1,48 @@
+const Fuse = require('fuse.js');
+
+async function getPages() {
+  try {
+    return fetch("https://zacharyc.site/pages.json")
+    .then(res => {
+      return res.json()
+    })
+    .then(res => {
+      return res
+    })
+  } catch (error) {
+    console.log(error);
+    return error.message;
+  }
+}
+
+async function search(query) {
+  const pages = await getPages();
+  const fuse = new Fuse(pages, {
+    keys: [
+      {
+        name: 'title',
+        weight: 5
+      },
+      {
+        name: 'description',
+        weight: 4
+      }, 
+      {
+        name: 'tags',
+        weight: 1
+      },
+    ],
+  });
+  const results = fuse.search(query, {limit: 12});
+  return results;
+}
+
+exports.handler = async (event, context) => {
+  const query = event.queryStringParameters.q;
+  const results = await search(query);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(results),
+  };
+};
