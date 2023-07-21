eec59c4c7f307c519d886e1fcbe6204adbc9305e
Zachary Crespin <Zachary.lights@gmail.com>
2023-07-21 10:38:25 -0700

    add quotes page

---
+++ src/quote/script.js
@@ -0,0 +1,11 @@
+fetch("/api/quote")
+  .then((response) => response.json())
+  .then((data) => {
+    document.getElementById("quoteQuote").innerText = `“${data.quote}”`;
+    if (data.author != "") {
+      document.getElementById("quoteAuthor").innerText = `— ${data.author}`;
+      document.title = `“${data.quote}” — ${data.author}`;
+    } else {
+      document.title = `“${data.quote}”`;
+    }
+  });
